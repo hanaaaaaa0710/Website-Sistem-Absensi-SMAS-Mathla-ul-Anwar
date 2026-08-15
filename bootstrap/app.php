b032bafe5ff3170130cc735405582c9cf6312351
hanaaaaaa0710 <burhanaratih0710@gmail.com>
2026-08-16 02:07:26 +0700

    Fix HTTPS assets behind Railway proxy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'role' => CheckRole::class,
             'user.status' => CheckUserStatus::class,
         ]);
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
